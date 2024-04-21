fd87f7bca8c743bc2e46a688d2ad3e9ac6f93371
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-04-21 23:03:52 +0530

    as

--- src/features/movieAction.js
+++ src/features/movieAction.js
@@ -3,7 +3,12 @@ import axios from "axios";
 
 export const showmovie = createAsyncThunk("showmovie", async (data) => {
     try {
-      const response = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1");
+      const response = await axios.get("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",{
+
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTg1YjhmMTliMTk2N2Y3MjA4NjY1M2JlNjg4N2VlYSIsInN1YiI6IjY0Mzc3MjEwMzdiM2E5MDA5NDlkMjhjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VIETsvx7YIo4HzfVPyQgecMwmE3Rzp3B9azSV8PUXy0"
+      }
+      });
       console.log(response.data);
       return response.data;
     } catch (error) {
